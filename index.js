3038fddf1271df7f953db79e60bcb8b347db8cc7
Sora Nakahara <skyfish1003@gmail.com>
2022-03-29 16:19:08 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const app = new Vue({
+    el: "#app",
+    data() {
+        return {
+            // 途中式はリスト形式でここで管理する
+            expressionList : [],
+            tmpInt : "",
+            tmpAns : 0,
+            tmpOp1 : 0,
+            tmpOperator : "+",
+            answer : "",
+        };
+    },
+    computed: {
+        // 途中式を文字列にしてinput form に渡す
+        expressionText: function(){
+            let res = "";
+            for(let i=0; i<this.expressionList.length; i++){
+                res += String(this.expressionList[i]);
+            }
+            return res;
+        }
+    },
+    methods: {
+        intButtonPushed: function(value){
+            this.tmpInt += String(value);
+            this.expressionList.push(value);
+        },
+        operatorButtonPushed: function(value){
+            // 演算子は連続では入力できない
+            if(typeof(this.expressionList[this.expressionList.length-1]) === "string"){
+                return;
+            }
+            // 演算子が押された時点で都度暫定的な答えを出す
+            // tmpIntをint型に変換, tmpInt初期化
+            let op2 = Number(this.tmpInt);
+            this.tmpInt = "";
+            this.tmpOperator = value;
+            this.tmpAns = fourArithmetic(this.tmpOp1, op2, this.tmpOperator);
+            console.log("tmp Answer is: " + this.tmpAns);
+
+            // 次の計算のためにtmpOp1（左辺）に暫定的な答えを代入しておく
+            this.tmpOp1 = this.tmpAns;
+
+            this.expressionList.push(" " + value + " ");
+        },
+        caluculate: function(){
+            // 最後が数字ならもう一度計算
+            if(typeof(this.expressionList[this.expressionList.length-1]) === "int"){
+                // tmpIntをint型に変換, tmpInt初期化
+                let op2 = Number(this.tmpInt);
+                this.tmpOperator = value;
+                this.tmpAns = fourArithmetic(this.tmpOp1, op2, this.tmpOperator);
+                console.log("tmp Answer is: " + this.tmpAns);
+            }
+            this.answer = this.tmpAns;
+            console.log("Final answer is: " + this.answer);
+
+            // 他の条件を初期化
+            this.expressionList = [];
+            this.tmpInt = "";
+            this.tmpAns = 0;
+            this.tmpOp1 = 0;
+            this.tmpOperator = "+";
+        },
+    }
+})
+
+function fourArithmetic(op1, op2, operator){
+    switch(operator){
+        case "+":
+            return op1 + op2;
+        case "-":
+            return op1 - op2;
+        case "*":
+            return op1 * op2;
+        case "/":
+            return op1 / op2;
+    }
+}
